6694ccf14286d234cb1229cea0a45b4e8657ee4c
Illya-Bilyk <bilikillya.i@gmail.com>
2022-08-16 21:10:21 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const refs = {
+  iframe: document.querySelector('iframe'),
+};
+
+const player = new Player(refs.iframe);
+
+const onPlay = data => {
+  const setTime = JSON.stringify(data);
+  localStorage.setItem('videoplayer-current-time', setTime);
+};
+
+function getLastTimeOnPlayer() {
+  const lastTimeValue = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+
+  if (lastTimeValue) player.setCurrentTime(lastTimeValue.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+getLastTimeOnPlayer();
