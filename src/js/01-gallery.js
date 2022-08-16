b39f930f0d07cbee26358058071e30f27920e9ac
Illya-Bilyk <bilikillya.i@gmail.com>
2022-08-16 17:52:51 +0300

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.addEventListener('click', galleryClick);
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join('');
+
+refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+function galleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+}
