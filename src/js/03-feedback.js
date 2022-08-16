f85b697fa4c7888d51c42be8b8d204f3da156b10
Illya-Bilyk <bilikillya.i@gmail.com>
2022-08-16 22:56:48 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const labelData = {};
+console.log('labelData', labelData);
+
+refs.form.addEventListener('input', e => {
+  e.preventDefault();
+
+  if (e.target.nodeName === 'INPUT') {
+    labelData.email = e.target.value;
+  }
+  if (e.target.nodeName === 'TEXTAREA') {
+    labelData.message = e.target.value;
+  }
+
+  setDataToLS(labelData);
+});
+
+function setDataToLS(labelData) {
+  const toString = JSON.stringify(labelData);
+  localStorage.setItem('feedback-form-state', toString);
+}
+
+// function fillTheFields() {
+//   const storageСheck = localStorage.getItem('feedback-form-state');
+//   if (!storageСheck) return;
+//   const parsedData = JSON.parse(storageСheck);
+
+//   refs.input.textContent = parsedData.email;
+// }
+// fillTheFields();
